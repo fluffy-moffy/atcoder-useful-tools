51781f3a6ed1d2c6d5be38ecb6c34b106e3c6f80
cuctus <90229589+moffuu@users.noreply.github.com>
2022-01-15 17:38:01 -0600

    Update checker.py
    v2 release!

--- checker.py
+++ checker.py
@@ -1,15 +1,51 @@
-import requests, html, sys
+#!/usr/bin/env python3
+#coding: utf-8
+
+import requests, html
+import sys
 from bs4 import BeautifulSoup
 
-url =  "https://atcoder.jp/contests/" + sys.argv[1] + "/submissions?f.Status=AC&f.User=" + sys.argv[2]
-html = requests.get(url)
+useage = "useage: python3 checker.py agc055 chokudai"
 
-soup = BeautifulSoup(html.content, "html.parser")
+logo = '''
+ ###                ###      ###                ##
+  ##                 ##       ##               ####
+  ##       ####      ##       ##      ####     ####
+  #####   ##  ##     ##       ##     ##  ##     ##
+  ##  ##  ######     ##       ##     ##  ##     ##
+  ##  ##  ##         ##       ##     ##  ##
+ ###  ##   #####    ####     ####     ####      ##
+'''
 
-X = []
+if len(sys.argv) < 3:
+    print(useage)
+    sys.exit()
 
-for tag in soup.select('td')[1::10]:
-    problemid = tag.text
-    X.append(problemid)
+def end():
+    time.sleep(0.5)
+    sys.exit(1)
 
-print(X)
+def checkcode(url, html):
+	x = []
+	soup = BeautifulSoup(html.content, "html.parser")
+	for tag in soup.select('td')[1::10]:
+	    problemid = tag.text
+	    x.append(problemid)
+	return x
+
+def cleancode(results):
+	for string in results:
+		print(string)
+
+def main():
+	try:
+		print(logo)
+		url = "https://atcoder.jp/contests/" + sys.argv[1] + "/submissions?f.Status=AC&f.User=" + sys.argv[2]
+		html = requests.get(url)
+		results = checkcode(url, html)
+		cleancode(results)
+	except KeyboardInterrupt:
+		end()
+
+if __name__ == '__main__':
+	main()
